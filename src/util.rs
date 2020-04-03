77aaa00f3ddef77c7865e00ab0cb63499f4f4b80
Aurorans Solis <primalucegd@gmail.com>
2020-04-03 14:11:50 -0700

    Started writing utilities, matching a handful of functions in mfloop.py and primenet.py.

---
+++ src/util.rs
@@ -0,0 +1,71 @@
+use std::fs::{remove_file, File};
+use std::io::{BufReader, BufWriter, Error as IoError, ErrorKind, Read, Result as IoResult, Write};
+use std::path::Path;
+
+fn lock_file(filename: &str) -> IoResult<()> {
+    let lockfile_name = format!("{}.lck", filename);
+    let _ = File::create(&lockfile_name)?;
+    Ok(())
+}
+
+fn unlock_file(filename: &str) -> IoResult<()> {
+    let lockfile_name = format!("{}.lck", filename);
+    if Path::new(&lockfile_name).exists() {
+        remove_file(&lockfile_name)?;
+    }
+    Ok(())
+}
+
+// Read a file without locking it
+fn read_nolock(filename: &str) -> IoResult<String> {
+    let lockfile_name = format!("{}.lck", filename);
+    if Path::new(&lockfile_name).exists() {
+        Err(IoError::new(
+            ErrorKind::Other,
+            format!("Found lockfile: {}", lockfile_name),
+        ))
+    } else {
+        let mut file_contents = String::new();
+        BufReader::new(File::open(filename)?).read_to_string(&mut file_contents)?;
+        Ok(file_contents)
+    }
+}
+
+// Read a list file and lock it
+fn read_list_lock(filename: &str) -> IoResult<Vec<String>> {
+    let lockfile_name = format!("{}.lck", filename);
+    if Path::new(&lockfile_name).exists() {
+        Err(IoError::new(
+            ErrorKind::Other,
+            format!("Found lockfile: {}", lockfile_name),
+        ))
+    } else {
+        let _ = File::create(&lockfile_name)?;
+        let mut file_contents = String::new();
+        BufReader::new(File::open(filename)?).read_to_string(&mut file_contents)?;
+        let lines = file_contents
+            .lines()
+            .map(|line| line.trim().to_string())
+            .collect::<Vec<_>>();
+        Ok(lines)
+    }
+}
+
+// Write a list to a file
+fn write_list(filename: &str, contents: &str) -> IoResult<()> {
+    let lockfile_name = format!("{}.lck", filename);
+    if Path::new(&lockfile_name).exists() {
+        Err(IoError::new(
+            ErrorKind::Other,
+            format!("Found lockfile: {}", lockfile_name),
+        ))
+    } else {
+        let _ = File::create(&lockfile_name)?;
+        let mut list_file = BufWriter::new(File::open(filename)?);
+        list_file.write_all(&[b'\n'])?;
+        list_file.write_all(contents.as_bytes())?;
+        list_file.write_all(&[b'\n'])?;
+        remove_file(&lockfile_name)?;
+        Ok(())
+    }
+}
