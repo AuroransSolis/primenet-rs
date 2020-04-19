2760324d98291451aa35ad627fd00e2e420bfb4f
Aurorans Solis <primalucegd@gmail.com>
2020-04-19 02:41:02 -0700

    Rewrote handling of options to include cleanup on failure.

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,27 @@ mod gpu72_runtime;
 mod primenet_runtime;
 mod util;
 
-use clap_handler::app::{Options, request_from_args};
-use primenet_runtime::primenet_runtime;
+use clap_handler::app::{request_from_args, Options};
+use gpu72_runtime::{gpu72_cleanup, gpu72_runtime};
+use primenet_runtime::{primenet_cleanup, primenet_runtime};
 
 fn main() {
     match request_from_args() {
         Ok(o) => {
-            // println!("Successfully parsed command line arguments:\n{:?}", o);
             println!("Successfully parsed command line arguments.");
-            match o {
-                Options::Primenet(primenet_options) => {
-                    primenet_runtime(primenet_options).expect("Primenet runtime returned an error:\n")
+            match (o, o.clone()) {
+                (Options::Primenet(primenet_options), Options::Primenet(clone)) => {
+                    if primenet_runtime(primenet_options).is_err() {
+                        primenet_cleanup(clone);
+                    }
                 }
-                Options::Gpu72(_) => println!("GPU to 72 not supported yet."),
+                (Options::Gpu72(gpu72_options), Options::Gpu72(clone)) => {
+                    if gpu72_runtime(gpu72_options).is_err() {
+                        // gpu72_cleanup(clone);
+                        println!("oh no");
+                    }
+                }
+                _ => unreachable!(),
             }
         }
         Err(e) => println!("{}", e),
